4f8dbdf3c70758e056bb2d7afd58c99d47054954
merrooo <138368815+merrooo@users.noreply.github.com>
2024-01-26 17:38:58 +0200

    Update deploy_data_drugs_type.py

--- deploy_data_drugs_type.py
+++ deploy_data_drugs_type.py
@@ -238,8 +238,8 @@ elif page =="- PREDICTION -":
 
   if ok:
 
-    # oe = OrdinalEncoder(categories=[['DrugY', 'drugC', 'drugX', 'drugA', 'drugB']])
-    # df['Drug'] = oe.fit_transform(df[['Drug']])
+    oe = OrdinalEncoder(categories=[['DrugY', 'drugC', 'drugX', 'drugA', 'drugB']])
+    df['Drug'] = oe.fit_transform(df[['Drug']])
 
     # df = pd.get_dummies(df, columns=['BP', 'Cholesterol','Sex'])
 
